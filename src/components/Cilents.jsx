238035f7d1902ba6d7bf91c50cdc2b919c49761c
Jirawat <jirawat.kri@ku.th>
2025-11-03 18:18:31 +0700

    update responsive

--- src/components/Cilents.jsx
+++ src/components/Cilents.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
-import  clientImgLogo_1 from "../assets/img/clients/client-01.png"
-import  clientImgLogo_2 from "../assets/img/clients/client-02.png"
-import  clientImgLogo_3 from "../assets/img/clients/client-03.png"
-import  clientImgLogo_4 from "../assets/img/clients/client-04.png"
-import  clientImgLogo_5 from "../assets/img/clients/client-05.png"
-import  clientImgLogo_6 from "../assets/img/clients/client-06.png"
-import  clientImgLogo_7 from "../assets/img/clients/client-07.png"
+import React from "react";
+import clientImgLogo_1 from "../assets/img/clients/client-01.png";
+import clientImgLogo_2 from "../assets/img/clients/client-02.png";
+import clientImgLogo_3 from "../assets/img/clients/client-03.png";
+import clientImgLogo_4 from "../assets/img/clients/client-04.png";
+import clientImgLogo_5 from "../assets/img/clients/client-05.png";
+import clientImgLogo_6 from "../assets/img/clients/client-06.png";
+import clientImgLogo_7 from "../assets/img/clients/client-07.png";
 
 function Cilents() {
-    const clientLogos = [
-            {img:clientImgLogo_1},
-            {img:clientImgLogo_2},
-            {img:clientImgLogo_3},
-            {img:clientImgLogo_4},
-            {img:clientImgLogo_5},
-            {img:clientImgLogo_6},
-            {img:clientImgLogo_7}
+  const clientLogos = [
+    { img: clientImgLogo_1 },
+    { img: clientImgLogo_2 },
+    { img: clientImgLogo_3 },
+    { img: clientImgLogo_4 },
+    { img: clientImgLogo_5 },
+    { img: clientImgLogo_6 },
+    { img: clientImgLogo_7 },
   ];
-  return  (
-    <div className='container md-auto max-w-[1920] py-10 text-center items-center mx-auto'>
-        <div className='text-[2.3rem] font-semibold text-[#4D4D4D]'>Our Cilents</div>
-        <div className='text-[1.5rem] font-semibold text-[#4D4D4D]'>we have been working with some Fortune 500+ cilent</div>
-        <div className='flex flex-col items-center my-10 md:flex-row md:justify-between'>
-            {
-                clientLogos.map((item) => (
-                    <img src={item.img}></img>
-
-                ))
-            }
-
-        </div>
+  return (
+    <div className="container md-auto max-w-[1920] py-10 text-center items-center justify-center mx-auto">
+      <div className="text-[2.3rem] font-semibold text-[#4D4D4D]">
+        Our Cilents
+      </div>
+      <div className="text-[1.5rem] font-semibold text-[#4D4D4D]">
+        we have been working with some Fortune 500+ cilent
+      </div>
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(0px,200px))] gap-y-5 justify-center justify-items-center my-10">
+        {clientLogos.map((item) => (
+          <div className="flex flex-row w-full items-center justify-center ">
+            <img src={item.img}></img>
+          </div>
+        ))}
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default Cilents
+export default Cilents;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import Headerimg from "../assets/img/header-img.png"
+import React from "react";
+import Headerimg from "../assets/img/header-img.png";
 function Header() {
   return (
-    <div className='bg-[#F5F7FA] h-auto md:h-[37.5rem] flex items-center mx-auto'>
-        <div className='container max-auto max-w-[1920px] mx-20 p-12 flex flex-col md:flex-row md:justify-between'>
-            <div>
-                <h1 className='text-[4rem] leading-[1] font-semibold text-[#4D4D4D]'>Lessons and insights<br /> <span className='text-[#4CAF4F]'>from 8 year</span> </h1>
-                <div className='text-[#717171] mt-4'>Where to grow your business as a photographer: site or social media?</div>
-                <div className='my-10 md:mx-0'><a className='inline-flex justify-center items-center py-3 px-8 bg-[#4CAF4F] text-white rounded-md' href="#logout">Register</a></div>
-            </div>
-            <div>
-                <img src={Headerimg}></img>
-            </div>
+    <div className="flex flex-col md:flex-row text-center md:text-left">
+      <div className="container mx-auto max-w-[1280px] md:flex md:justify-between md:items-center my-10">
+        <div className="mx-4">
+          <div className="text-[1rem] font-semibold">
+            Branding | Image making
+          </div>
+          <div className="text-[5rem] font-semibold">My awesome</div>
+          <div className="text-[5rem] font-semibold">portfolio</div>
+          <div className="text-[1rem]">
+            And I made it myself! Yes. In Figma with Anima
+          </div>
         </div>
-
-
+        <div className="flex justify-center">
+          <img
+            className="my-10 md:w-[31rem]"
+            src={Headerimg}
+            alt="Header Logo"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
